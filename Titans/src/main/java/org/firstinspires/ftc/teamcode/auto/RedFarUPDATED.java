82eb62a4b867125641b33a19ab060189ce45e55a
Mars Robotics <oswaldaviles358@gmail.com>
2026-02-06 15:16:30 -0600

    More tuned titan auto, finish tuning the auto and add more time to armdown

---
+++ Titans/src/main/java/org/firstinspires/ftc/teamcode/auto/RedFarUPDATED.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Config
+@Autonomous(name = "RedFar", group = "Autonomous")
+public class RedFarUPDATED extends LinearOpMode {
+    public class arm {
+        public Action Armdown;
+        private DcMotor arm;
+
+        public arm(HardwareMap hardwareMap) {
+            arm = hardwareMap.get(DcMotorEx.class, "launch");
+            arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        }
+
+        public class Armdown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    arm.setPower(-0.8);
+                    sleep(3000);
+                    arm.setPower(0);
+                    initialized = true;
+                }
+
+                return arm.isBusy();
+            }
+        }
+
+        public Action Armdown() {
+            return new Armdown();
+        }
+    }
+
+
+
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d( 63,12, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        arm arm = new arm(hardwareMap);
+
+        // vision here that outputs position
+        int visionOutputPosition = 1;
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .strafeToLinearHeading(new Vector2d(47,12),Math.toRadians(180))
+                .splineTo(new Vector2d(-24, 24), Math.toRadians(135));
+
+        Action tab2 = tab1.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-63.4,8),Math.toRadians(90))
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        tab1.build(),
+                        arm.Armdown(),
+                        tab2
+                )
+        );
+    }
+}
